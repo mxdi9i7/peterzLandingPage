7bd37dd89e1e6072c7793b43ac9b7c3fa10c8063
mxdi9i7 <peter.zheng88228@gmail.com>
2016-09-29 21:38:01 -0400

    asdf

--- js/index.js
+++ js/index.js
@@ -15,6 +15,21 @@
 })(jQuery);
 
 $(document).ready(function() {
+    $('#homeTab').show(1000);
+    $('#aboutTab').hide();
+    $('#portTab').hide();
+    $('#workTab').hide();
+    $('#contractTab').hide();
+  $('form input').focus(function() {
+    $('form input').css('color', 'white');
+  })
+  $('form textarea').focus(function() {
+    $('form textarea').css('color', 'white')
+  })
+
+
+
+
   $('#homeLink').click(function() {
     $('#homeTab').show();
     $('#aboutTab').hide();
@@ -50,4 +65,13 @@ $(document).ready(function() {
     $('#workTab').hide();
     $('#contractTab').show();
   })
+  $('#aboutLinkToContact').click(function() {
+    $('ul li').addClass('active');
+    $('#homeTab').hide();
+    $('#aboutTab').hide();
+    $('#portTab').hide();
+    $('#workTab').hide();
+    $('#contractTab').show();
+  })
+
 })
