1beb16d7a870e8856aa193f7cdc004bbc214f777
mxdi9i7 <peter.zheng88228@gmail.com>
2016-10-12 22:55:27 -0400

    asdf

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@ $field_name = $_POST['name'];
 $field_email = $_POST['email'];
 $field_message = $_POST['message'];
 
-$mail_to = '443527350@qq.com';
+$mail_to = 'peter.zheng88228@gmail.com';
 $subject = 'Message from a site visitor '.$field_name;
 
 $body_message = 'From: '.$field_name."\n";
